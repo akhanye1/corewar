4bc82ff224c61053f7461c8c77b338b755c4c59d
Joseph Ngoma <jngoma@e5r6p2.wtc.co.za>
2017-08-19 14:40:44 +0200

    name test

--- ft_corewar.h
+++ ft_corewar.h
@@ -6,7 +6,7 @@
 /*   By: akhanye <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 23:40:00 by akhanye           #+#    #+#             */
-/*   Updated: 2017/08/17 10:44:42 by akhanye          ###   ########.fr       */
+/*   Updated: 2017/08/19 14:38:16 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct		s_conv
 typedef struct		s_asm
 {
 	struct s_conv	*conv;
+	struct header_s	header;
 }					t_asm;
 
 int		ft_fileok(char *filename);
--- main.c
+++ main.c
@@ -6,23 +6,63 @@
 /*   By: akhanye <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 21:04:42 by akhanye           #+#    #+#             */
-/*   Updated: 2017/08/17 11:16:18 by akhanye          ###   ########.fr       */
+/*   Updated: 2017/08/19 14:38:12 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_corewar.h"
 
+static char		*copy_quote(char *line, int *len)
+{
+	int i;
+	int start;
+	int l;
+
+	i = -1;
+
+	while(line[++i] != '"')
+		;
+	start = i;
+	l = 0;
+	while (line[++i] && line[i] != '"')
+		l++;
+	*len = l;
+	return (line + start);
+}
+
+static int		get_header(int fd, t_asm *header)
+{
+	char 	*line;
+	int		ex;
+	int		len;
+
+	data->header->magic = COREWAR_EXEC_MAGIC;
+	ft_bzero(data->header->prog_name, PROG_NAME_LENGTH);
+	ft_bzero(data->header->comment, COMMENT_CHAR);
+	ex = 0;
+	len = 0;
+	while (get_next_line(fd, &line) > 0 && ex < 2)
+	{
+		if (ft_strncmp(line, ".name", ft_strlen(".name")) == 0)
+		{
+			ft_strncpy(data->header->prog_name, copy_quote(line, &len), len);
+			ft_putendl(data->header->prog_name);
+		}
+	}
+	return (ex == 2);	
+}
+
 static int		convert_file(int fd)
 {
-	char	*line;
-	t_asm	data;
+	char		*line;
+	t_asm		data;
 
 	line = NULL;
 	data.conv = NULL;
-	while (get_next_line(fd, &line) > 0)
+	get_header(fd, &data);
+	/*while (get_next_line(fd, &line) > 0)
 	{
-		
-	}
+	}*/
 	return (1);
 }
 
