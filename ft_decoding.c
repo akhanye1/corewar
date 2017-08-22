99bf0511c81ae92a42c6261c31e131c9c6ef95fd
Joseph L. Ngoma <30995482+Kid-Seven-7@users.noreply.github.com>
2017-08-22 15:25:17 +0200

    adding decoding

--- ft_decoding.c
+++ ft_decoding.c
@@ -1,37 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_get_encoding.c                                  :+:      :+:    :+:   */
+/*   ft_decoding.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jngoma <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/22 10:34:00 by jngoma            #+#    #+#             */
-/*   Updated: 2017/08/22 10:34:02 by jngoma           ###   ########.fr       */
+/*   Created: 2017/08/22 13:45:45 by jngoma            #+#    #+#             */
+/*   Updated: 2017/08/22 13:45:47 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_corewar.h"
 
-unsigned char		ft_get_encoding(char *str)
+char 		*ft_decoding(char *str)
 {
 	int					i;
 	int					j;
 	char				**grid;
-	static char			ret[9];
+	static char			ret[4];
 
 	i = -1;
-	j = 0;
-	ret[8] = '\0';
-	while (i < 7)
-		ret[++i] = '0';
-	i = -1;
+	j = -1;
+	ret[3] = '\0';
 	grid = ft_strsplit(ft_strdefix(str, ' '), ',');
 	while (++i < 3)
 	{
-		ret[j] = (grid[i][0] == 'r') ? '0' : '1';
-		ret[j + 1] = (grid[i][0] == 'r') ? '1' : 0;
-		ret[j + 1] = (grid[i][0] == '%') ? '0' : '1';
-		j += 2;
+		if (grid[i][0] == 'r')
+			ret[++j] = '1';
+		else if (grid[i][0] == '%')
+			ret[++j] = '2';
+		else
+			ret[++j] = '3';
 	}
-	return (bintodec(ret));
+	return (ret);
 }
