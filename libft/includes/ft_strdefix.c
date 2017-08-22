66bd5dbd0303ab0cdd27bad35d1ce63a7d334fee
Joseph L. Ngoma <30995482+Kid-Seven-7@users.noreply.github.com>
2017-08-22 12:45:49 +0200

    done with the encoding byte code

--- libft/includes/ft_strdefix.c
+++ libft/includes/ft_strdefix.c
@@ -6,26 +6,25 @@
 /*   By: jngoma <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/21 18:51:44 by jngoma            #+#    #+#             */
-/*   Updated: 2017/08/22 07:22:51 by jngoma           ###   ########.fr       */
+/*   Updated: 2017/08/22 08:37:25 by jngoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	*ft_defix(char *str, int n)
+char	*ft_strdefix(char *str, int c)
 {
 	char	*newstr;
 	int		i;
 	int		j;
 	int		len;
 
-	i = n - 1;
+	i = -1;
+	while (str[++i] && str[i] != c);
 	j = -1;
 	len = ft_strlen(str);
-	newstr = (char *)malloc((len - i) + 1);
-	while (str[i++] != '\0')
-	{
+	newstr = ft_strnew(len - i);
+	while (str[++i] != '\0')
 		newstr[++j] = str[i];
-	}
 	return (newstr);
 }
