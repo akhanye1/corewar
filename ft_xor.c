0e35ab43d95d308e57b510674f368ec157ab7b5d
Mushagi MAYIBO <mmayibo@e5r7p2.wethinkcode.co.za>
2017-08-26 10:04:49 +0200

    altering xor

--- ft_xor.c
+++ ft_xor.c
@@ -6,13 +6,13 @@
 /*   By: jbadenho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/25 14:15:57 by jbadenho          #+#    #+#             */
-/*   Updated: 2017/08/25 14:15:57 by jbadenho         ###   ########.fr       */
+/*   Updated: 2017/08/26 10:00:53 by mmayibo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_corewar.h"
 
-int	ft_xor(t_conv *instruct)
+int	ft_xor(t_conv *instruct, int total_bytes)
 {
 	char	*defix;
 	char	**split;
@@ -27,6 +27,9 @@ int	ft_xor(t_conv *instruct)
 	instruct->hasencoding = 1;
 	instruct->encoding = ft_get_encoding(instruct->line);
 	instruct->n_params = 3;
+	instruct->index = total_bytes + 1;
+	instruct->indir_bytes = IND_SIZE;
+	instruct->dir_bytes = IND_SIZE;
 	decode = ft_decoding(instruct->line);
 	fill_params(instruct, split, decode);
 	i = -1;
