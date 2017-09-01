b8fa65c5f1042839477232b58f97b22542cd759c
Mushagi MAYIBO <mmayibo@e4r13p1.wtc.co.za>
2017-09-01 09:01:14 +0200

    adding filename

---
+++ main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhanye <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/27 21:04:42 by akhanye           #+#    #+#             */
+/*   Updated: 2017/09/01 09:00:41 by mmayibo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_corewar.h"
+
+int				main(int ac, char **av)
+{
+	int		fd;
+	char	debug;
+
+	if (((ac == 3 && ft_strcmp(av[2], "-v")) &&
+				(ac <= 3 && ft_strcmp(av[2], "-v")))
+				|| (ac == 1))
+	{
+		ft_putstr("Usage: asm <filename> [-v]\n");
+		return (0);
+	}
+	debug = (ac == 3) ? 1 : 0;
+	if (!(fd = ft_fileok(av[1])))
+	{
+		ft_putstr("Usage: asm filename\nInvalid file\n");
+		return (0);
+	}
+	if (!convert_file(fd, debug, av[1]))
+		ft_putstr("Error converting file\n");
+	close(fd);
+	return (0);
+}
+
